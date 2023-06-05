1b3a547088ba6da2bff5dfae628f1df8983c705b
AbduTech <abdulqudoospk737@gmail.com>
2023-06-05 18:08:26 +0500

    installed tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './index.css'
-import './App.css'
+// import './App.css'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home>
+      </Home>
     </>
   )
 }
